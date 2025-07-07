f881b01f04487e9a114f88a59ddabe5ee57eb14a
olebergen <ole.bergen.92@gmail.com>
2025-07-07 10:52:28 +0200

    use manifest v2

--- background.js
+++ background.js
@@ -1,39 +1,20 @@
-const MAX_AGE_MS = 24 * 60 * 60 * 1000; // 24 hours
-
-async function closeInactiveTabs() {
-  const tabs = await browser.tabs.query({});
+function closeOldTabs() {
   const now = Date.now();
+  const cutoff = now - 24 * 60 * 60 * 1000; // 24 hours
 
-  for (const tab of tabs) {
-    if (!tab.active && now - tab.lastAccessed > MAX_AGE_MS) {
-      try {
-        await browser.tabs.remove(tab.id);
-      } catch (err) {
-        console.warn(`Could not close tab ${tab.id}: ${err}`);
+  chrome.tabs.query({}, (tabs) => {
+    for (const tab of tabs) {
+      if (tab.lastAccessed && tab.lastAccessed < cutoff) {
+        chrome.tabs.remove(tab.id);
       }
     }
-  }
+  });
 }
 
-// Handle alarm
-browser.alarms.onAlarm.addListener((alarm) => {
-  if (alarm.name === 'cleanup-tabs') {
-    closeInactiveTabs();
-  }
-});
+closeOldTabs();
 
-// Set or reset alarm on startup
-browser.runtime.onStartup.addListener(() => {
-  browser.alarms.create('cleanup-tabs', {
-    delayInMinutes: 1,
-    periodInMinutes: 60,
-  });
-});
+chrome.alarms.create('cleanup', { periodInMinutes: 5 });
 
-// Also initialize when installed
-browser.runtime.onInstalled.addListener(() => {
-  browser.alarms.create('cleanup-tabs', {
-    delayInMinutes: 1,
-    periodInMinutes: 60,
-  });
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === 'cleanup') closeOldTabs();
 });
